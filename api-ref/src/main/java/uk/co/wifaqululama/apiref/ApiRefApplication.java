10d26397e1b207fa097c5d6208adc3ca80ab4b4e
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-11-21 21:46:53 +0000

    Cleanup

---
+++ api-ref/src/main/java/uk/co/wifaqululama/apiref/ApiRefApplication.java
@@ -0,0 +1,13 @@
+package uk.co.wifaqululama.apiref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRefApplication.class, args);
+    }
+
+}
---
+++ api-ref/src/test/java/uk/co/wifaqululama/apiref/ApiRefApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.wifaqululama.apiref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
